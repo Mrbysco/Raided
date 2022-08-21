3eb755337f59a3969868a4ffddd3bac5e6872224
Mrbysco <Mrbysco@users.noreply.github.com>
2022-08-21 03:50:28 +0200

    Update ElectromancerRenderer.java

--- src/main/java/com/mrbysco/raided/client/renderer/ElectromancerRenderer.java
+++ src/main/java/com/mrbysco/raided/client/renderer/ElectromancerRenderer.java
@@ -3,15 +3,11 @@ package com.mrbysco.raided.client.renderer;
 import com.mrbysco.raided.Raided;
 import com.mrbysco.raided.client.ClientHandler;
 import com.mrbysco.raided.client.model.ElectromancerModel;
-import com.mrbysco.raided.client.model.InquisitorModel;
 import com.mrbysco.raided.entity.Electromancer;
-import com.mrbysco.raided.entity.Inquisitor;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.client.renderer.entity.layers.ItemInHandLayer;
 import net.minecraft.resources.ResourceLocation;
-
-import java.util.List;
 
 public class ElectromancerRenderer extends MobRenderer<Electromancer, ElectromancerModel> {
 	private static final ResourceLocation TEXTURE = new ResourceLocation(Raided.MOD_ID, "textures/entity/illager/electromancer.png");
