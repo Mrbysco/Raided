3b92a6898c23b0c1c4b209201dd11daffc089293
Mrbysco <Mrbysco@users.noreply.github.com>
2025-09-03 20:10:37 +0200

    Remove unused import

--- src/main/java/com/mrbysco/raided/entity/Incinerator.java
+++ src/main/java/com/mrbysco/raided/entity/Incinerator.java
@@ -36,7 +36,6 @@ import net.minecraft.world.entity.monster.RangedAttackMob;
 import net.minecraft.world.entity.npc.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
-import net.minecraft.world.entity.projectile.SmallFireball;
 import net.minecraft.world.entity.raid.Raider;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
