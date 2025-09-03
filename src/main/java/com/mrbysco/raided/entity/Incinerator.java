0bef6926b0fc0c9ae202551b2d13abfc71caaaff
Mrbysco <Mrbysco@users.noreply.github.com>
2025-09-03 22:20:50 +0200

    -

--- src/main/java/com/mrbysco/raided/entity/Incinerator.java
+++ src/main/java/com/mrbysco/raided/entity/Incinerator.java
@@ -8,7 +8,6 @@ import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.damagesource.DamageSource;
