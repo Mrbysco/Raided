3c8a44045baebd1bf119f199a3df0f178b344f7c
Mrbysco <Mrbysco@users.noreply.github.com>
2026-04-17 20:50:49 +0200

    Change item models

--- src/main/java/com/mrbysco/raided/datagen/client/RaidedItemModelsProvider.java
+++ src/main/java/com/mrbysco/raided/datagen/client/RaidedItemModelsProvider.java
@@ -5,9 +5,8 @@ import com.mrbysco.raided.registry.RaidedRegistry;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
 import net.minecraft.client.data.models.ModelProvider;
-import net.minecraft.client.data.models.model.ItemModelUtils;
+import net.minecraft.client.data.models.model.ModelTemplates;
 import net.minecraft.data.PackOutput;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.SpawnEggItem;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -21,7 +20,7 @@ public class RaidedItemModelsProvider extends ModelProvider {
 	protected void registerModels(BlockModelGenerators blockModels, ItemModelGenerators itemModels) {
 		for (DeferredHolder<Item, ? extends Item> item : RaidedRegistry.ITEMS.getEntries()) {
 			if (item.get() instanceof SpawnEggItem) {
-				itemModels.itemModelOutput.accept(item.get(), ItemModelUtils.plainModel(ResourceLocation.withDefaultNamespace("item/pillager_spawn_egg")));
+				itemModels.generateFlatItem(item.get(), ModelTemplates.FLAT_ITEM);
 			}
 		}
 	}
